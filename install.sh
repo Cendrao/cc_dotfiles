a2032e32b8f270999e9e7bd5bc0d00ef9d5bfb37
Matheus Cendrão <matheuscendrao@gmail.com>
2020-06-22 18:01:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ then
       git checkout "$(git describe --abbrev=0 --tags)"
       echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
       echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+      source ~/.bashrc
       asdf plugin-add ruby
       asdf install ruby 2.7.1
       ;;
